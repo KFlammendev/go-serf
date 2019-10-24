fe6f09b4aae1f79115e9ef99c363afc955b9f22d
Klaus <flammen.dev@gmail.com>
2019-10-24 16:46:56 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrow slider-arrow__left" src="../img/arrow-left.png" alt="1"></img>',
+        nextArrow: '<img class="slider-arrow slider-arrow__right" src="../img/arrow-right.png" alt="1"></img>',
+    });
+
+  
+});
