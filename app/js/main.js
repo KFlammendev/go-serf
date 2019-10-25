d59559d1cf64af0ac91bf8c0e27e6038659c270b
Klaus <flammen.dev@gmail.com>
2019-10-26 00:39:00 +0300

    third lesson

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,13 @@ $(function(){
         fade: true,
         prevArrow: '<img class="slider-arrow slider-arrow__left" src="../img/arrow-left.png" alt="1"></img>',
         nextArrow: '<img class="slider-arrow slider-arrow__right" src="../img/arrow-right.png" alt="1"></img>',
+        asNavFor: '.slider-dots'
+    });
+
+    $('.slider-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: 'header__slider', //связываю этот элемент с верхним, а верхний с этим
     });
 
   
